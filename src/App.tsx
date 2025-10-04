5f978b6215d9fee6ddd3dfca50d41ded1a22b6f5
petermenzies <petermenzies@ucsb.edu>
2025-10-03 21:02:25 -0700

    remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import './App.css'
 
