509f84569ed1ebe0d112db9024184edb08fd0f4a
petermenzies <petermenzies@ucsb.edu>
2025-10-03 20:57:52 -0700

    remove example map marker

--- src/App.tsx
+++ src/App.tsx
@@ -15,11 +15,6 @@ function App() {
           attribution='&copy; <a href="https://www.mapbox.com/about/maps/">Mapbox</a> &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
           url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=${import.meta.env.VITE_MAPBOX_TOKEN}`}
         />
-        <Marker position={[34.419, -119.700]}>
-          <Popup>
-            Santa Barbara, CA
-          </Popup>
-        </Marker>
       </MapContainer>
     </div>
   )
